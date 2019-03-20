c5bc1365c826c59ba173a339885dccc2f02e54ac
Sharon Lee <sharonallilee@gmail.com>
2019-03-20 12:07:59 -0400

    Add Next and Back button to the base component

--- src/App.js
+++ src/App.js
@@ -30,32 +30,68 @@ const rectangleInnerStyle = {
   zIndex:'2',
 }
 
+const nextStyle = {
+  position:'absolute',
+  bottom: '5px',
+  right: '5px'
+}
+
+const backStyle = {
+  position:'absolute',
+  bottom: '5px',
+  left: '5px'
+}
+
 // Need to figure out which component is going to change the progress bar step
 // Change progress bar to red '#FF342A'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {step:0, date: new Date()};
+    this.state = {step:1, date: new Date()};
+    this.handleNext = this.handleNext.bind(this);
+    this.handlePrev = this.handlePrev.bind(this);
   }
 
   
+  handleNext() {
+    this.setState((prevState) => {return {step: prevState.step + 1}});
+  }
+  
+  handlePrev() {
+    this.setState({step: this.state.step - 1});
+  }
+
   render() {
     const handleStep = (self) => (step) => {
-      // self.setState((prevState) => {return {step: prevState.step + 1}});
-      self.setState({step: step})
+      self.setState({step: step});
     }
     const handleDateChange = (self) => (date) => {
-      self.setState({date:date})
+      self.setState({date:date});
     }
-    
+
+    const stepComponent = () => {
+      switch(this.state.step) {
+        case 1:
+          return <Step1 handleStep={handleStep(this)} handleDate={handleDateChange(this)} initDate={this.state.date} />
+        case 2:
+          return <Step2 handleStep={handleStep(this)}/>
+        default:
+          console.log(this.state.step)
+          return <p>No Step</p>
+      }
+    }
+      
     return (
       <div>
          <div style={logoStyle}>MeetVite</div>
          <Progress totalSteps={4} currentStep={this.state.step}/>
          <div style={rectangleOuterStyle} className="rounded progress"></div>
          <div style={rectangleInnerStyle} className="rounded progress">
-            <Step1 step={this.state.step} handleStep={handleStep(this)} handleDate={handleDateChange(this)} initDate={this.state.date} />
+            {stepComponent()}
+            <button type="button" style={nextStyle} onClick={this.handleNext}>Next</button>
+            <button type="button" style={backStyle} onClick={this.handlePrev}>Back</button>
          </div>
+         
       </div>
     );
   }
